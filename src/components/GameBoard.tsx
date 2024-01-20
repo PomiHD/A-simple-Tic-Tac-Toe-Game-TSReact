417f99c6b004b0a7219b7b3bf032d8719b7cfa59
PomiHD <wsgddjy@live.com>
2024-01-21 02:04:40 +1000

    updated to latest Node.js

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,19 +1,29 @@
-﻿import {useState} from "react";
+import { useState } from "react";
 
-const initailGameBoard = [
+const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
-  const [gameBoard,setGameBoard]=useState(initailGameBoard)
-  function handleSelectSquare(rowIndex,colIndex){
-    setGameBoard((prevGameBoard)=> {
-      const updatedBoard = [...prevGameBoard].map(innerArr=> [...innerArr]);
-      updatedBoard[rowIndex][colIndex]= 'X';
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  // console.log(typeof gameBoard);
+  // console.log(gameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [...prevGameBoard].map((innerArr) => [...innerArr]);
+      // console.log("prevGameBoard: " + prevGameBoard);
+      // console.log(typeof [...prevGameBoard]);
+      // console.log("[...prevGameBoard]: " + [...prevGameBoard]);
+      // console.log("Before updatedBoard: " + updatedBoard);
+      updatedBoard[rowIndex][colIndex] = "X";
+      // console.log("After updatedBoard: " + updatedBoard);
+      // console.log("{\n}");
       return updatedBoard;
-    })
+    });
   }
+
   return (
     <ol id={"game-board"}>
       {gameBoard.map((row, rowIndex) => (
@@ -21,7 +31,9 @@ export default function GameBoard() {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick ={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
