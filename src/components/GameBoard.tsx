1331f9f13fe93af2dad13aade4f7bf16a21ab2d6
PomiHD <wsgddjy@live.com>
2024-01-20 20:31:36 +1000

    80. Best Practice: Updating Object State Immutably

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,17 +1,27 @@
-﻿const initailGameBoard = [
+﻿import {useState} from "react";
+
+const initailGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard,setGameBoard]=useState(initailGameBoard)
+  function handleSelectSquare(rowIndex,colIndex){
+    setGameBoard((prevGameBoard)=> {
+      const updatedBoard = [...prevGameBoard].map(innerArr=> [...innerArr]);
+      updatedBoard[rowIndex][colIndex]= 'X';
+      return updatedBoard;
+    })
+  }
   return (
     <ol id={"game-board"}>
-      {initailGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick ={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
