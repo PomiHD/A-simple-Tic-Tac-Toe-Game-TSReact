a970a258bfdfb50149ba26be7dc336892430819c
PomiHD <wsgddjy@live.com>
2024-01-20 18:25:04 +1000

    78. User Input & Two-Way-Binding

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,27 +1,32 @@
 ﻿import React from "react";
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
-    // now we want to update this component when save player info, thus we use useSate()
-    const [isEditing, setIsEditing] = useState(false);
+export default function Player({ initalName, symbol }) {
+  // now we want to update this component when save player info, thus we use useSate()
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initalName);
+  function handleEditClick() {
+    // setIsEditing(!isEditing) // React is scheduling these sate update, not happen immediately
+    setIsEditing((edting) => !edting); //but we can use (edting)=>!edting, this can get the latest state of isEditing
+  }
+  function handleChange(event) {
+    // console.log(event)
+    setPlayerName(event.target.value);
+  }
 
-    function handleEditClick() {
-        // setIsEditing(!isEditing) // React is scheduling these sate update, not happen immediately
-        setIsEditing((edting)=>!edting) //but we can use (edting)=>!edting, this can get the latest state of isEditing
-       
-    }
-
-    let playerName = <span className={"player-name"}>{name}</span>;
-    if(isEditing){
-        playerName = <input type ="text" required value={name}/>
-    }
-    return (
-      <li>
-        <span className={"player"}>
-          {playerName}
-          <span className={"player-symbol"}> {symbol}</span>
-        </span>
-        <button onClick={handleEditClick}>{isEditing?"Save":"Edit"}</button>
-      </li>
+  let editablePlayerName = <span className={"player-name"}>{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
     );
+  }
+  return (
+    <li>
+      <span className={"player"}>
+        {editablePlayerName}
+        <span className={"player-symbol"}> {symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
