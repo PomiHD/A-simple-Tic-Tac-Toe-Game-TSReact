d1872da5d2659407d7775a2b57d051c3807d9d5b
PomiHD <wsgddjy@live.com>
2024-01-20 17:18:38 +1000

    74. Concept Repetition: Working with State

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Player from "./components/Player";
+
 function App() {
-
-
   return (
-      <main>
-        <div id = "game-container">
-          <ol id ="players">
-              <Player name={"Player 1"} symbol={"X"} />
-              <Player name={"Player 2"} symbol={"O"} />
-          </ol>
-            game board
-        </div>
-          logic
-      </main>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name={"Player 1"} symbol={"X"} />
+          <Player name={"Player 2"} symbol={"O"} />
+        </ol>
+        game board
+      </div>
+      logic
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,13 +1,25 @@
 ﻿import React from "react";
+import { useState } from "react";
 
-export default function Player({name,symbol}) {
-    return( <li>
-                <span className={"player"}>
-              <span className={"player-name"}>
-                {name}
-              </span>
-                <span className={"player-symbol"}> {symbol}</span>
-                </span>
-        <button>Edit</button>
-    </li>);
+export default function Player({ name, symbol }) {
+    // now we want to update this component when save player info, thus we use useSate()
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true)
+    }
+
+    let playerName = <span className={"player-name"}>{name}</span>;
+    if(isEditing){
+        playerName = <input type ="text" required/>
+    }
+    return (
+      <li>
+        <span className={"player"}>
+          {playerName}
+          <span className={"player-symbol"}> {symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>Edit</button>
+      </li>
+    );
 }
