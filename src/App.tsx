f6c027cd02dec11214932f1a4d5c885c214b6989
PomiHD <wsgddjy@live.com>
2024-01-23 03:58:07 +1000

    add Helper

--- src/App.tsx
+++ src/App.tsx
@@ -6,55 +6,13 @@ import { useState } from "react";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
+import {
+  deriveActivePlayer,
+  deriveGameBoard,
+  deriveWinner,
+  PLAYERS,
+} from "./Helper";
 
-const PLAYERS = {
-  X: "Player 1",
-  O: "Player 2",
-};
-const INITIAL_GAME_BOARD = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-function deriveActivePlayer(gameTurns: string | any[]) {
-  let currentPlayer = "X";
-  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
-    currentPlayer = "O";
-  }
-  return currentPlayer;
-}
-function deriveGameBoard(gameTurns: any[]) {
-  let gameBoard = [...INITIAL_GAME_BOARD.map((innerArr) => [...innerArr])];
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
-  return gameBoard;
-}
-function deriveWinner(
-  gameBoard: any[][],
-  players: { [x: string]: any; X?: string; O?: string },
-) {
-  let winner: any;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol =
-      gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =
-      gameBoard[combination[2].row][combination[2].column];
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
-    }
-  }
-  return winner;
-}
 function App() {
   const [players, setPlayers] = useState(PLAYERS);
   // const [activePlayer, setActivePlayer] = useState("X");
